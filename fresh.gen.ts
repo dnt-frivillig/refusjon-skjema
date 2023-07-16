d21ae044b6f488248ae145f0b04b007e178f9aed
Max Brosnahan <maximilianbrosnahan@gmail.com>
2023-07-16 19:47:45 +0200

    Upgrade 1.2

--- fresh.gen.ts
+++ fresh.gen.ts
@@ -2,7 +2,6 @@
 // This file SHOULD be checked into source version control.
 // This file is automatically updated during development when running `dev.ts`.
 
-import config from "./deno.json" assert { type: "json" };
 import * as $0 from "./routes/[name].tsx";
 import * as $1 from "./routes/_app.tsx";
 import * as $2 from "./routes/api/joke.ts";
@@ -30,7 +29,6 @@ const manifest = {
     "./islands/SignatureCanvas.tsx": $$4,
   },
   baseUrl: import.meta.url,
-  config,
 };
 
 export default manifest;
